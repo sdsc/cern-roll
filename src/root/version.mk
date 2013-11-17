cefed82e5221d3bba28985024c8a28b2cdc81185
root <root@gcn-20-88.sdsc.edu>
2013-11-17 12:40:51 -0800

    root version.mk

---
+++ src/root/version.mk
@@ -0,0 +1,12 @@
+PKGROOT		= /opt/cern/root
+NAME		= root
+VERSION		= 5.34.11
+RELEASE		= 1
+TARBALL_POSTFIX	= tar.gz
+
+ROOT_NAME = root
+ROOT_VERSION=5.34.11
+
+SRC_SUBDIR	= root
+
+TGZ_PKGS = $(ROOT_NAME)_v$(ROOT_VERSION).source.$(TARBALL_POSTFIX)
