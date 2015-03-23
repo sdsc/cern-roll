924c6a86535e7632393d3d2d0b9f3eb9cb10b094
Jerry Greenberg <jpg@comet-devel.local>
2015-03-23 16:10:30 -0700

    put in auto versioning

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME	= cern
-RELEASE		= 0
-COLOR		= firebrick
+ROLLNAME        = cern
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT	= $(CURDIR)
+REDHAT.ROOT     = $(PWD)
