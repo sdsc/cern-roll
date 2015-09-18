13b22d90ad2d93d06bcebcc686f65e2cb51af48c
Jim Hayes <jhayes@sdsc.edu>
2015-09-18 15:30:24 -0700

    Add missing MODULE_LOAD_CC. Resolves APS-198.

--- src/root/version.mk
+++ src/root/version.mk
@@ -1,15 +1,16 @@
-NAME               = root
-VERSION            = 6.02.05
-RELEASE            = 0
-PKGROOT            = /opt/cern/root
+NAME           = sdsc-root
+VERSION        = 6.02.05
+RELEASE        = 1
+PKGROOT        = /opt/cern/root
 
-SRC_SUBDIR         = root
+SRC_SUBDIR     = root
 
-SOURCE_NAME        = $(NAME)
-SOURCE_VERSION     = $(VERSION)
-SOURCE_SUFFIX      = tar.gz
-SOURCE_PKG         = $(SOURCE_NAME)_v$(SOURCE_VERSION).source.$(SOURCE_SUFFIX)
-SOURCE_DIR         = $(SOURCE_NAME)-$(SOURCE_VERSION)
+SOURCE_NAME    = root
+SOURCE_SUFFIX  = tar.gz
+SOURCE_VERSION = $(VERSION)
+SOURCE_PKG     = $(SOURCE_NAME)_v$(SOURCE_VERSION).source.$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_NAME)-$(SOURCE_VERSION)
 
-TAR_GZ_PKGS        = $(SOURCE_PKG)
-RPM.EXTRAS         = AutoReq:No
+TAR_GZ_PKGS    = $(SOURCE_PKG)
+
+RPM.EXTRAS     = AutoReq:No
